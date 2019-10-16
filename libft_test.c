7cdc279a6e4150c433d638942ca76ebac18c619b
Willehard Korander <willehard@gmail.com>
2019-10-16 12:23:16 +0300

    Added tests

--- libft_test.c
+++ libft_test.c
@@ -6,7 +6,7 @@
 /*   By: wkorande <wkorande@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 21:27:33 by wkorande          #+#    #+#             */
-/*   Updated: 2019/10/16 11:18:57 by wkorande         ###   ########.fr       */
+/*   Updated: 2019/10/16 12:17:49 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@
 
 #define BUFSIZE 50
 
-int tests_run = 0;
+int num_tests = 0;
 
 static char *test_ft_atoi()
 {
 	char *str = "-123";
-	mu_assert("error: ft_strlen failed!", atoi(str) == ft_atoi(str));
+	mu_assert("ft_atoi", "error: ft_atoi failed!", atoi(str) == ft_atoi(str));
 	return 0;
 }
 
@@ -38,35 +38,83 @@ static char *test_ft_bzero()
 	bzero(str1, 20);
 	ft_bzero(str2, 20);
 
-	mu_assert("error: ft_bzero failed!", memcmp(str1, str2, BUFSIZE) == 0);
+	mu_assert("ft_bzero", "error: ft_bzero failed!", memcmp(str1, str2, BUFSIZE) == 0);
 	return 0;
 }
 
 static char *test_ft_isalnum()
 {
 	char c = '1';
-	mu_assert("error: ft_isalnum failed!", ft_isalnum(c) == 1);
+	mu_assert("ft_isalnum", "error: ft_isalnum failed!", ft_isalnum(c) == 1);
 	return (0);
 }
 
 static char *test_ft_isalpha()
 {
 	char c = 'a';
-	mu_assert("error: ft_isalnum failed!", ft_isalpha(c) == 1);
+	mu_assert("ft_isalpha", "error: ft_isalpha failed!", ft_isalpha(c) == 1);
 	return (0);
 }
 
 static char *test_ft_isascii()
 {
 	char c = '1';
-	mu_assert("error: ft_isalnum failed!", ft_isascii(c) == 1);
+	mu_assert("ft_isascii", "error: ft_isascii failed!", ft_isascii(c) == 1);
 	return (0);
 }
 
 static char *test_ft_isdigit()
 {
 	char c = '1';
-	mu_assert("error: ft_isalnum failed!", ft_isdigit(c) == 1);
+	mu_assert("ft_isdigit", "error: ft_isdigit failed!", ft_isdigit(c) == 1);
+	return (0);
+}
+
+static char *test_ft_isprint()
+{
+	char c = '1';
+	mu_assert("ft_isprint", "error: ft_isprint failed!", ft_isprint(c) == 1);
+	return (0);
+}
+
+static char *test_ft_memccpy()
+{
+	char src[] = "test basic memccpy !";
+	char buf1[20];
+	char buf2[20];
+
+	memccpy(buf1, src, 'x', 10);
+	ft_memccpy(buf2, src, 'x', 10);
+
+	mu_assert("ft_memccpy", "error: ft_memccpy failed!", memcmp(buf1, buf2, 10) == 0);
+	return (0);
+}
+
+static char *test_ft_memcpy()
+{
+	char src[] = "test basic memcpy !";
+	char buf1[19];
+	char buf2[19];
+
+	memcpy(buf1, src, 10);
+	ft_memcpy(buf2, src, 10);
+
+	mu_assert("ft_memcpy", "error: ft_memcpy failed!", memcmp(buf1, buf2, 10) == 0);
+	return (0);
+}
+
+static char *test_ft_memset()
+{
+	char buf1[BUFSIZE];
+	char buf2[BUFSIZE];
+
+	memset(buf1, 'X', BUFSIZE);
+	memset(buf2, 'X', BUFSIZE);
+
+	memset(buf1, 'O', BUFSIZE);
+	ft_memset(buf2, 'O', BUFSIZE);
+
+	mu_assert("ft_memset", "error: ft_memset failed!", memcmp(buf1, buf2, 10) == 0);
 	return (0);
 }
 
@@ -74,13 +122,13 @@ static char *test_ft_strcmp()
 {
 	char *s1 = "hello world!";
 	char *s2 = "hello world!";
-	mu_assert("error: ft_strcmp failed!", strcmp(s1, s2) == ft_strcmp(s1, s2));
+	mu_assert("ft_strcmp", "error: ft_strcmp failed!", strcmp(s1, s2) == ft_strcmp(s1, s2));
 	return 0;
 }
 
 static char *test_ft_strlen()
 {
-	mu_assert("error: ft_strlen failed!", strlen("123") == ft_strlen("123"));
+	mu_assert("ft_strlen", "error: ft_strlen failed!", strlen("123") == ft_strlen("123"));
 	return 0;
 }
 
@@ -96,7 +144,7 @@ static char *test_ft_strcpy()
 	strcpy(dst1, src);
 	ft_strcpy(dst2, src);
 
-	mu_assert("error: ft_strcpy failed!", strcmp(dst1, dst2) == 0);
+	mu_assert("ft_strcpy", "error: ft_strcpy failed!", strcmp(dst1, dst2) == 0);
 	return 0;
 }
 
@@ -108,6 +156,11 @@ static char *all_tests() {
 	mu_run_test(test_ft_isalpha);
 	mu_run_test(test_ft_isascii);
 	mu_run_test(test_ft_isdigit);
+	mu_run_test(test_ft_isprint);
+
+	mu_run_test(test_ft_memccpy);
+	mu_run_test(test_ft_memcpy);
+	mu_run_test(test_ft_memset);
 
 	mu_run_test(test_ft_strcmp);
 	mu_run_test(test_ft_strlen);
@@ -124,9 +177,9 @@ int main(void/*int argc, char **argv*/)
 	else
 	{
 		const char* user = getenv("USER");
-		printf("ALL TESTS PASSED!\nGreat job, %s!\n", user);
+		printf("\033[0;32mALL OK!\033[0m\nGreat job, %s!\n", user);
 	}
-	printf("Tests run: %d\n", tests_run);
+	printf("Tests run: %d\n", num_tests);
 
 	return result != 0;
 }
--- minunit.h
+++ minunit.h
@@ -6,16 +6,17 @@
 /*   By: wkorande <wkorande@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 21:25:50 by wkorande          #+#    #+#             */
-/*   Updated: 2019/10/15 21:27:06 by wkorande         ###   ########.fr       */
+/*   Updated: 2019/10/16 12:16:53 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINUNIT_H
 # define MINUNIT_H
 
-# define mu_assert(message, test) do { if (!(test)) return message; } while (0)
-# define mu_run_test(test) do { char *message = test(); tests_run++; if (message) return message; } while (0)
+# define mu_assert(testname, message, test) do { if (!(test)) return message; else printf("%s:\t \033[0;32mOK\033[0m\n", testname); } while (0)
+# define mu_run_test(test) do { char *message = test(); num_tests++; if (message) return message; } while (0)
+//# define mu_test_ok(test) printf("test: \033[0;32mOK\033[0m\n")
 
-extern int tests_run;
+extern int num_tests;
 
 #endif
